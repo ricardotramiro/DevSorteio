1397c4ff3ab0140d8c5ab4947eef972020df4d57
ricardotramiro <ricardoteixeiraramiro@gmail.com>
2025-03-27 15:16:02 -0300

    Operadores de comparação

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function generateNumber(){
+function generateNumber() {
+  const min = Math.ceil(document.querySelector(".input-min").value);
+  const max = Math.floor(document.querySelector(".input-max").value);
 
-   const min = Math.ceil( document.querySelector(".input-min").value)
-   const max = Math.floor( document.querySelector(".input-max").value)
+  if (min >= max) {
+    alert("O valor minimo tem que ser Menor que o valor máximo");
+  } else {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-   const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-   alert(result)
+    alert(result);
+  }
 }
